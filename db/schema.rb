3692919262ef10cc043a1493310dbd88e83b543d
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-08 15:08:07 +0300

    Specify the referenced table in entities migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_143123) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_095858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categorizations", force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entity_id"], name: "index_categorizations_on_entity_id"
+    t.index ["group_id"], name: "index_categorizations_on_group_id"
+  end
+
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.string "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_entities_on_author_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.text "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_143123) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categorizations", "entities"
+  add_foreign_key "categorizations", "groups"
+  add_foreign_key "entities", "users", column: "author_id"
+  add_foreign_key "groups", "users"
 end
