2ba9be0fafa2e6f644278b6760ba832c668714df
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-08 19:38:58 +0300

    Add unit tests for the group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :entities, through: :categorizations
+
+  #  validations
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :icon, presence: true
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    user = User.create(name: 'Gachero', email: 'gacheruuuu@gmail.com', password: '123456',
+                       password_confirmation: '123456')
+    @group = Group.create(name: 'Sports', icon: 'link/goes/here')
+    @group.user = user
+  end
+
+  it 'is not valid without a name' do
+    @group.name = nil
+    expect(@group).to_not be_valid
+  end
+
+  it 'is not valid if the name is too long' do
+    @group.name = 'a' * 41
+    expect(@group).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    @group.icon = nil
+    expect(@group).to_not be_valid
+  end
 end
