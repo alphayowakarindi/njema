efb7d43aa7909f96c30b70de352b6c181876f3cc
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-08 18:12:15 +0300

    Add unit tests for the entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,8 @@
 class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :goups, through: :categorizations
+
+  #  validations
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :amount, presence: true
 end
--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Entity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    user = User.create(name: 'Gachero', email: 'gacheru@gmail.com', password: '123456',
+                       password_confirmation: '123456')
+    @entity = Entity.create(name: 'Radio', amount: 300)
+    @entity.author = user
+  end
+
+  it 'is not valid without a name' do
+    @entity.name = nil
+    expect(@entity).to_not be_valid
+  end
+
+  it 'is not valid if the name is too long' do
+    @entity.name = 'a' * 41
+    expect(@entity).to_not be_valid
+  end
+
+  it 'is not valid without an amount' do
+    @entity.amount = nil
+    expect(@entity).to_not be_valid
+  end
 end
