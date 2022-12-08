78e7ca27c2602c3db7d2cf6b09585ee387bf138c
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-08 16:09:56 +0300

    Change amount column type to integer

--- db/migrate/20221208094102_create_entities.rb
+++ db/migrate/20221208094102_create_entities.rb
@@ -2,7 +2,7 @@ class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
       t.string :name
-      t.string :amount
+      t.integer :amount
       t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
