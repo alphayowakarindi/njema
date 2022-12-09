7dc3dad37a8056f5bf56b7be67f0d6ba5dea8b76
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-09 09:40:09 +0300

    Add the logic to attach an image when creating a group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,10 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :entities, through: :categorizations
 
+  has_one_attached :icon do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
   #  validations
   validates :name, presence: true, length: { maximum: 40 }
   validates :icon, presence: true
