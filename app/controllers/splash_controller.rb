3f922769b7d9961f276a91f4856bbd75ab27fe50
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-13 10:35:26 +0300

    Redirect visit to splash to homepage if login is true

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,4 +1,14 @@
 class SplashController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :login_status
+
   def index; end
+
+  private
+
+  def login_status
+    return unless user_signed_in?
+
+    redirect_to groups_path
+  end
 end
